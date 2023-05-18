16e5bb37476374e153d3526819ef2a502a5f18a7
lixindong <lixindong@keep.com>
2023-05-19 03:18:45 +0800

    全局唤起快捷键
    Signed-off-by: lixindong <lixindong@keep.com>

---
+++ XCAChatGPTMac/business/main/AppShortcuts.swift
@@ -0,0 +1,43 @@
+//
+//  AppShortcuts.swift
+//  Found
+//
+//  Created by lixindong on 2023/5/19.
+//
+
+import SwiftUI
+
+struct AppShortcuts: View {
+    let options: [AppShortcutOption] = [.init(name: "双击 ⌘", id: "command"),
+                                        .init(name: "双击 ⌥", id: "option"),
+                                        .init(name: "双击 ⌃", id: "control"),
+                                        .init(name: "自定义", id: "custom")]
+    @AppStorage("appShortcutOption") var appShortcutOption: String = "option"
+
+    var body: some View {
+        HStack {
+            Picker("", selection: $appShortcutOption) {
+                ForEach(options, id: \.id) { option in
+                    Text(LocalizedStringKey(option.name))
+                }
+            }
+            .onChange(of: appShortcutOption, perform: { newValue in
+                KeyMonitorManager.shared.updateModifier(appShortcutKey())
+            })
+            .pickerStyle(.menu)
+            .frame(width: 150)
+
+        }
+    }
+}
+
+struct AppShortcutOption {
+    let name: String
+    let id: String
+}
+
+struct AppShortcuts_Previews: PreviewProvider {
+    static var previews: some View {
+        AppShortcuts()
+    }
+}
